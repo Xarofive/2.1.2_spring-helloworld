3c2a78f08f2398438402fa662a997cb0feac51b9
Denis <de.pronin@gmail.com>
2021-06-16 14:46:45 +0300

    Result

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+
+        boolean helloWorldPair = bean == bean2;
+        boolean catPair = bean3 == bean4;
+
+        System.out.println(helloWorldPair);
+        System.out.println(catPair);
+
+
     }
 }
